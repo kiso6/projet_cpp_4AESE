0c77f5a8a58022137e1df191cabec5d9447fbd83
kiso6 <kiliansoual@outlook.fr>
2023-04-03 22:44:31 +0200

    +led class+buzzer class

---
+++ tesla/constants.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+#include <Arduino.h>
+
+#define PHARE_G D5
+#define PHARE_D D6
+#define KLAXON D7
+
+#endif
--- tesla/grove_class.h
+++ tesla/grove_class.h
@@ -1,18 +1,23 @@
+#ifndef GROVE_CLASS_H
+#define GROVE_CLASS_H
+
+//Basic grove class
 class Grove {
 protected :
-  const int pin1;
-  const int pin1mode;
-  const int pin2;
-  const int pin2mode;
-
+  //default initialized to 0
+  const int pin1{0};
+  const int pin1mode{0};
+  const int pin2{0};
+  const int pin2mode{0};
 public :
+  //constructors
   Grove(int p1, int p1m);
   Grove(int p1, int p1m, int p2, int p2m);
-  virtual Init() = 0;
-  
+  //virtual foncitons to define
+  //virtual void Init() = 0;
 };
 
-class Capteur : public Grove {
+/*class Capteur : public Grove {
 
 
 };
@@ -21,24 +26,36 @@ class Ultrason : public Capteur {
 
 
 };
-
+*/
 class Actionneur : public Grove {
-
-
+public:
+  Actionneur(int p1,int p1m);
+  Actionneur(int p1,int p1m,int p2,int p2m);
 };
 
 class Led : public Actionneur {
-
-
+public: 
+  Led(int pin, int mode);
+  void Init(int);
+  void clignotant(int);
+  void phare(bool);
 };
 
+class Buzzer: public Actionneur{
+public:
+  Buzzer(int pin, int mode);
+  void Init(int);
+  void klaxonner(int,bool);
+};
+
+/*
 class Servo : public Actionneur {
 
 
-};
+};*/
 
 
-
+#endif
 
 
 
