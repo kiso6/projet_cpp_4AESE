75f55ea41090687581a1b767ff8193893ac2ef0c
raphaeltessier <tessier.raphael@laposte.net>
2023-04-03 17:24:44 +0200

    setup fichier orga

---
+++ tesla/grove_class.cpp
@@ -0,0 +1,1 @@
+#include "grove_class.h"
--- tesla/grove_class.h
+++ tesla/grove_class.h
@@ -42,12 +42,4 @@ class Servo : public Actionneur {
 
 
 
-void setup() {
-  // put your setup code here, to run once:
 
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
