6d74db682fc8d1939b0a5e2b192b30bf615ceec9
raphaeltessier <tessier.raphael@laposte.net>
2023-04-03 18:24:16 +0200

    pin config .txt

--- tesla/grove_class.cpp
+++ tesla/grove_class.cpp
@@ -1,1 +1,2 @@
 #include "grove_class.h"
+
--- tesla/grove_class.h
+++ tesla/grove_class.h
@@ -6,8 +6,8 @@ protected :
   const int pin2mode;
 
 public :
-  Grove(int p1, int p1m);
-  Grove(int p1, int p1m, int p2, int p2m);
+  Grove(int p1, int p1m): pin1(p1), pin1mode(p1m) {};
+  Grove(int p1, int p1m, int p2, int p2m): pin1(p1), pin1mode(p1m), pin2(p2), pin1mode(p1m){};;
   virtual Init() = 0;
   
 };
@@ -36,6 +36,11 @@ class Servo : public Actionneur {
 
 
 };
+
+class Moteur : public actionneur {
+
+  
+}
 
 
 
